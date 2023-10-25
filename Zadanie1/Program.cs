50040068c9a1a37d21e66ab4848033de94906fea
user <user@ADMIN>
2023-10-25 10:22:46 +0300

    Добавьте файлы проекта.

---
+++ Zadanie1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите первое число: ");
+        if (double.TryParse(Console.ReadLine(), out double number1))
+        {
+            Console.Write("Введите второе число: ");
+            if (double.TryParse(Console.ReadLine(), out double number2))
+            {
+                if (number1 == number2)
+                {
+                    Console.WriteLine("Два числа равны.");
+                }
+                else if (number1 > number2)
+                {
+                    Console.WriteLine("Первое число больше второго.");
+                }
+                else
+                {
+                    Console.WriteLine("Первое число меньше второго.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка при вводе второго числа.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Ошибка при вводе первого числа.");
+        }
+    }
+}
+
+
+
+
+
+
